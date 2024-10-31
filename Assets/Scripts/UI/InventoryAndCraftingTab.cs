34dc43964fff62186abde9ca36177e5b5a97b1fb
Jooglorystar <121430174+Jooglorystar@users.noreply.github.com>
2024-10-31 18:56:48 +0900

    Refactor: Scripts merge

--- Assets/Scripts/UI/InventoryAndCraftingTab.cs
+++ Assets/Scripts/UI/InventoryAndCraftingTab.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public enum InventoryState
 {
@@ -6,32 +7,96 @@ public enum InventoryState
     Crafting
 }
 
+public enum PageState
+{
+    Page1,
+    Page2
+}
+
 public class InventoryAndCraftingTab : MonoBehaviour
 {
     [SerializeField] private GameObject inventoryTab;
     [SerializeField] private GameObject craftingTab;
 
+    [SerializeField] private GameObject NextPageButton;
+    [SerializeField] private GameObject PreviousPageButton;
+
+    [SerializeField] private GameObject Inventory1Page;
+    [SerializeField] private GameObject Inventory2Page;
+    [SerializeField] private GameObject Crafting1Page;
+    [SerializeField] private GameObject Crafting2Page;
+
+
     static public InventoryState State;
 
     public void Start()
     {
+        PreviousPageButton.SetActive(false);
+        Inventory2Page.SetActive(false);
+        Crafting2Page.SetActive(false);
         State = InventoryState.Inventory;
     }
+
+    public void OnInventoryUI(InputAction.CallbackContext context)
+    {
+
+    }
+
+    // 인벤토리 탭 버튼
     public void OnInventoryTab()
     {
-        if (inventoryTab.activeInHierarchy) return;
-
-        craftingTab.SetActive(false);
-        inventoryTab.SetActive(true);
-        State = InventoryState.Inventory;
+        if (State == InventoryState.Crafting)
+        {
+            craftingTab.SetActive(false);
+            inventoryTab.SetActive(true);
+            State = InventoryState.Inventory;
+        }
     }
 
+    // 크래프팅 탭 버튼
     public void OnCraftingTab()
     {
-        if (craftingTab.activeInHierarchy) return;
+        if (State == InventoryState.Inventory)
+        {
+            inventoryTab.SetActive(false);
+            craftingTab.SetActive(true);
+            State = InventoryState.Crafting;
+        }
+    }
 
-        inventoryTab.SetActive(false);
-        craftingTab.SetActive(true);
-        State = InventoryState.Crafting;
+    // 2페이지로 이동
+    public void GoNextPage()
+    {
+        if (State == InventoryState.Inventory)
+        {
+            Inventory2Page.SetActive(true);
+            Inventory1Page.SetActive(false);
+        }
+        else if (State == InventoryState.Crafting)
+        {
+            Crafting2Page.SetActive(true);
+            Crafting1Page.SetActive(false);
+        }
+
+        PreviousPageButton.SetActive(true);
+        NextPageButton.SetActive(false);
+    }
+
+    // 1페이지로 이동
+    public void GoPreviousPage()
+    {
+        if (State == InventoryState.Inventory)
+        {
+            Inventory2Page.SetActive(false);
+            Inventory1Page.SetActive(true);
+        }
+        else if (State == InventoryState.Crafting)
+        {
+            Crafting2Page.SetActive(false);
+            Crafting1Page.SetActive(true);
+        }
+
+        NextPageButton.SetActive(true);
+        PreviousPageButton.SetActive(false);
     }
 }
