ee53d61ce7bdfa43e9f12da70e60464149ddf160
Zzondy-Unity <abdos230@naver.com>
2024-11-01 11:27:29 +0900

    Feature Interaction, Asset shooter

--- Assets/Scripts/Player/Interact.cs
+++ Assets/Scripts/Player/Interact.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public interface IInteractable
@@ -7,7 +9,75 @@ public interface IInteractable
     string GetInteractPromptDescription();
 }
 
+//Searching
 public class Interact : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject promptPanel;
+    [SerializeField] private GameObject descriptionPanel;
+    [SerializeField] private TextMeshProUGUI itemNamePrompt;
+    [SerializeField] private TextMeshProUGUI itemDescriptionPrompt;
+    [SerializeField] private LayerMask interactableLayerMask;
+
+    [SerializeField] float interactionDistance = 3;
+    [SerializeField] float interactionRate = 0.5f;
+
+    private Camera cam;
+    private bool hasSearched = false;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        promptPanel.SetActive(false);
+        descriptionPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!hasSearched)
+        {
+            Searching();
+            Invoke("SearchAgain", interactionRate);
+        }
+    }
+
+    private void Searching()
+    {
+        Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0));
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, interactionDistance, interactableLayerMask))
+        {
+            if(hit.collider.TryGetComponent<IInteractable>(out IInteractable interactable))
+            {
+                hasSearched = true;
+
+                SetNamePrompt(interactable.GetInteractPromptName());
+                SetDescriptionPrompt(interactable.GetInteractPromptDescription());
+            }
+        }
+        else
+        {
+            promptPanel.SetActive(false);
+            descriptionPanel.SetActive(false);
+        }
+    }
+
+    private void SearchAgain()
+    {
+        hasSearched = false;
+    }
+
+    private void SetNamePrompt(string name)
+    {
+        promptPanel.SetActive(true);
+        itemNamePrompt.text = name;
+    }
+
+    private void SetDescriptionPrompt(string description)
+    {
+        descriptionPanel.SetActive(true);
+        itemDescriptionPrompt.text = description;
+    }
+
+
 }
