3a45a57346f7e1d00e7032e984eb9532f7cd3b84
Jooglorystar <121430174+Jooglorystar@users.noreply.github.com>
2024-11-01 09:40:29 +0900

    Feat: ItemSlot.cs
    item Data에 따라 아이콘이 변경되는 기능 추가, 현재 크래프트슬롯 쪽에 오류가 나지만, 별도의 CraftingSlot을 만들어 해결할 예정

--- Assets/Scripts/UI/ItemSlot.cs
+++ Assets/Scripts/UI/ItemSlot.cs
@@ -1,5 +1,41 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemSlot : MonoBehaviour
 {
+    [SerializeField] private ItemSO itemData;
+
+    [SerializeField] private Image slotIcon;
+    [SerializeField] private TextMeshProUGUI itemCountText;
+
+    private Outline outline;
+
+    public int index;
+    public int itemCount;
+
+    private void Awake()
+    {
+        outline = GetComponent<Outline>();
+    }
+
+    private void Start()
+    {
+        // юс╫ц
+        if (itemData != null) Set();
+        else Clear();
+    }
+    public void Set()
+    {
+        slotIcon.gameObject.SetActive(true);
+        slotIcon.sprite = itemData.icon;
+        itemCountText.text = itemCount > 1? itemCount.ToString() : string.Empty;
+    }
+
+    public void Clear()
+    {
+        itemData = null;
+        slotIcon.gameObject.SetActive(false);
+        itemCountText.text = string.Empty;
+    }
 }
