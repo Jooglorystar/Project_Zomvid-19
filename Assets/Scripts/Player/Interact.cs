1b2b300f9daf3ebf96dca454ca2bbf26046282cd
jsj2518 <joseph2518@naver.com>
2024-11-01 09:38:12 +0900

    Feature : Environment Manager

--- Assets/Scripts/Player/Interact.cs
+++ Assets/Scripts/Player/Interact.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public interface IInteractable
 {
     void OnInteraction();
+    string GetInteractPromptName();
+    string GetInteractPromptDescription();
 }
 
 public class Interact : MonoBehaviour
